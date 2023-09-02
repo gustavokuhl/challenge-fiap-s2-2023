702e4af8cc774c3563590f4671893fb92a1fac72
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2023-09-02 18:21:36 -0300

    add files

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Exampple app listening at http://localhost:${port}`)
+});
+
+app.get('/foo', (req, res) => {
+    res.json({"foo": "bar"});
+});
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.post("/bar", (req, res) => {
+    var body = req.body;
+    res.send(req.body.foo);
+});
